75907d55a27b7dd3cd8633976751a6475c0bf867
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-09-26 13:20:37 -0400

    Map Engine Test Site(Twin Pine) 1.2  /l.svgscaleoverlay.js

--- l.svgscaleoverlay.js
+++ l.svgscaleoverlay.js
@@ -60,30 +60,9 @@ L.SVGScaleOverlay = L.Class.extend({
               console.log(this.svgOverlay._map.getPanes().overlayPane);
             
             //  document.appendChild(svgImg.documentElement);
-               this._svg  = this.svgOverlay._map.getPanes().overlayPane.appendChild(svgImg.documentElement);\this._g = this._svg.g;
-        
-        if (!this.isLeafletVersion1()) {
-            L.DomUtil.addClass(this._g, 'leaflet-zoom-hide');
-        }
-        var size = this._map.getSize();
-        this._svgSize = size;
-        this._svg.setAttribute('width', size.x);
-        this._svg.setAttribute('height', size.y);
-       
-        console.log(this._map.getSize());
-        this._svg.appendChild(this._g);
-
-           
-
-
-        this._groundZoom = this._map.getZoom();
-
-        this._shift = new L.Point(0, 0);
-        this._lastZoom = this._map.getZoom();
-
-        var bounds = this._map.getBounds();
-        this._lastTopLeftlatLng = new L.LatLng(bounds.getNorth(), bounds.getWest()); ////this._initialTopLeft     = this._map.layerPointToLatLng(this._lastLeftLayerPoint)
-        this._g = this._svg.g;
+               this._svg  = this.svgOverlay._map.getPanes().overlayPane.appendChild(svgImg.documentElement);
+            //   console.log(svg);
+            this._g = this._svg.g;
         
         if (!this.isLeafletVersion1()) {
             L.DomUtil.addClass(this._g, 'leaflet-zoom-hide');
@@ -106,7 +85,6 @@ L.SVGScaleOverlay = L.Class.extend({
 
         var bounds = this._map.getBounds();
         this._lastTopLeftlatLng = new L.LatLng(bounds.getNorth(), bounds.getWest()); ////this._initialTopLeft     = this._map.layerPointToLatLng(this._lastLeftLayerPoint);
-            //   console.log(svg);
         });
         // var xmlns = "http://www.w3.org/2000/svg";
         // this._svg = document.createElementNS(xmlns, "svg");
