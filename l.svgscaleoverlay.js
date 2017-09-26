2e6863b392b701b821893ae8aff245fee355e615
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-09-26 12:46:04 -0400

    Map Engine Test Site(Twin Pine) 1.2  /l.svgscaleoverlay.js

--- l.svgscaleoverlay.js
+++ l.svgscaleoverlay.js
@@ -57,7 +57,7 @@ L.SVGScaleOverlay = L.Class.extend({
             if (error) throw error;
               svgImg_doc = svgImg.documentElement;
               console.log(svgImg.documentElement);
-            //   console.log(this._map);
+              console.log(this);
             
              document.appendChild(svgImg.documentElement);
               
