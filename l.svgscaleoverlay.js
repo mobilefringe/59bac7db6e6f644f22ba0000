7a0427df17f9f5a85efacb32e4b3e815175c9f8b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-09-26 13:36:38 -0400

    Map Engine Test Site(Twin Pine) 1.2  /l.svgscaleoverlay.js

--- l.svgscaleoverlay.js
+++ l.svgscaleoverlay.js
@@ -62,7 +62,7 @@ L.SVGScaleOverlay = L.Class.extend({
             //  document.appendChild(svgImg.documentElement);
                this._svg  = this.svgOverlay._map.getPanes().overlayPane.appendChild(svgImg.documentElement);
             //   this._svg.addClass('leaflet-zoom-hide');
-               console.log(  this._svg);
+               console.log(this._svg);
         });
         // var xmlns = "http://www.w3.org/2000/svg";
         // this._svg = document.createElementNS(xmlns, "svg");
