c4265c3e796bcfc7bd87da8e43130aaec8d2770f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-09-27 15:53:53 -0400

    Map Engine Test Site(Twin Pine) 1.2  /l.svgscaleoverlay.js

--- l.svgscaleoverlay.js
+++ l.svgscaleoverlay.js
@@ -91,8 +91,8 @@ L.SVGScaleOverlay = L.Class.extend({
         console.log("resize");
     },
     moveEnd: function (e) {
-        console.log(this);
-         console.log(this._map.getZoomScale(this._map._zoom),this._map._zoom);
+        // console.log(this);
+        //  console.log(this._map.getZoomScale(this._map._zoom),this._map._zoom);
         var bounds = this._map.getBounds();
         var topLeftLatLng      = new L.LatLng(bounds.getNorth(), bounds.getWest());
         var topLeftLayerPoint  = this._map.latLngToLayerPoint(topLeftLatLng);
