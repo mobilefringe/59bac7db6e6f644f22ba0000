9b8e35943d2c7f0fed57ec96b53c97af2a52dead
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-09-27 13:14:01 -0400

    Map Engine Test Site(Twin Pine) 1.2  /svgoverlay_sk.js

--- svgoverlay_sk.js
+++ svgoverlay_sk.js
@@ -1,91 +1,141 @@
-o.ImageOverlay = o.Layer.extend({
-        options: {
-            opacity: 1,
-            alt: "",
-            interactive: !1
-        },
-        initialize: function(t, e, i) {
-            this._url = t,
-            this._bounds = o.latLngBounds(e),
-            o.setOptions(this, i)
-        },
-        onAdd: function() {
-            this._image || (this._initImage(),
-            this.options.opacity < 1 && this._updateOpacity()),
-            this.options.interactive && (o.DomUtil.addClass(this._image, "leaflet-interactive"),
-            this.addInteractiveTarget(this._image)),
-            this.getPane().appendChild(this._image),
-            this._reset()
-        },
-        onRemove: function() {
-            o.DomUtil.remove(this._image),
-            this.options.interactive && this.removeInteractiveTarget(this._image)
-        },
-        setOpacity: function(t) {
-            return this.options.opacity = t,
-            this._image && this._updateOpacity(),
-            this
-        },
-        setStyle: function(t) {
-            return t.opacity && this.setOpacity(t.opacity),
-            this
-        },
-        bringToFront: function() {
-            return this._map && o.DomUtil.toFront(this._image),
-            this
-        },
-        bringToBack: function() {
-            return this._map && o.DomUtil.toBack(this._image),
-            this
-        },
-        setUrl: function(t) {
-            return this._url = t,
-            this._image && (this._image.src = t),
-            this
-        },
-        getAttribution: function() {
-            return this.options.attribution
-        },
-        getEvents: function() {
-            var t = {
-                zoom: this._reset,
-                viewreset: this._reset
-            };
-            return this._zoomAnimated && (t.zoomanim = this._animateZoom),
-            t
-        },
-        getBounds: function() {
-            return this._bounds
-        },
-        getElement: function() {
-            return this._image
-        },
-        _initImage: function() {
-            var t = this._image = o.DomUtil.create("img", "leaflet-image-layer " + (this._zoomAnimated ? "leaflet-zoom-animated" : ""));
-            t.onselectstart = o.Util.falseFn,
-            t.onmousemove = o.Util.falseFn,
-            t.onload = o.bind(this.fire, this, "load"),
-            this.options.crossOrigin && (t.crossOrigin = ""),
-            t.src = this._url,
-            t.alt = this.options.alt
-        },
-        _animateZoom: function(t) {
-            var e = this._map.getZoomScale(t.zoom)
-              , i = this._map._latLngToNewLayerPoint(this._bounds.getNorthWest(), t.zoom, t.center);
-            o.DomUtil.setTransform(this._image, i, e)
-        },
-        _reset: function() {
-            var t = this._image
-              , e = new o.Bounds(this._map.latLngToLayerPoint(this._bounds.getNorthWest()),this._map.latLngToLayerPoint(this._bounds.getSouthEast()))
-              , i = e.getSize();
-            o.DomUtil.setPosition(t, e.min),
-            t.style.width = i.x + "px",
-            t.style.height = i.y + "px"
-        },
-        _updateOpacity: function() {
-            o.DomUtil.setOpacity(this._image, this.options.opacity)
-        }
-    }),
-    o.imageOverlay = function(t, e, i) {
-        return new o.ImageOverlay(t,e,i)
-    }
+/*
+ * L.ImageOverlay is used to overlay images over the map (to specific geographical bounds).
+ */
+
+L.ImageOverlay = L.Class.extend({
+	includes: L.Mixin.Events,
+
+	options: {
+		opacity: 1
+	},
+
+	initialize: function (url, bounds, options) { // (String, LatLngBounds, Object)
+		this._url = url;
+		this._bounds = L.latLngBounds(bounds);
+
+		L.setOptions(this, options);
+	},
+
+	onAdd: function (map) {
+		this._map = map;
+
+		if (!this._image) {
+			this._initImage();
+		}
+
+		map._panes.overlayPane.appendChild(this._image);
+
+		map.on('viewreset', this._reset, this);
+
+		if (map.options.zoomAnimation && L.Browser.any3d) {
+			map.on('zoomanim', this._animateZoom, this);
+		}
+
+		this._reset();
+	},
+
+	onRemove: function (map) {
+		map.getPanes().overlayPane.removeChild(this._image);
+
+		map.off('viewreset', this._reset, this);
+
+		if (map.options.zoomAnimation) {
+			map.off('zoomanim', this._animateZoom, this);
+		}
+	},
+
+	addTo: function (map) {
+		map.addLayer(this);
+		return this;
+	},
+
+	setOpacity: function (opacity) {
+		this.options.opacity = opacity;
+		this._updateOpacity();
+		return this;
+	},
+
+	// TODO remove bringToFront/bringToBack duplication from TileLayer/Path
+	bringToFront: function () {
+		if (this._image) {
+			this._map._panes.overlayPane.appendChild(this._image);
+		}
+		return this;
+	},
+
+	bringToBack: function () {
+		var pane = this._map._panes.overlayPane;
+		if (this._image) {
+			pane.insertBefore(this._image, pane.firstChild);
+		}
+		return this;
+	},
+
+	setUrl: function (url) {
+		this._url = url;
+		this._image.src = this._url;
+	},
+
+	getAttribution: function () {
+		return this.options.attribution;
+	},
+
+	_initImage: function () {
+		this._image = L.DomUtil.create('img', 'leaflet-image-layer');
+
+		if (this._map.options.zoomAnimation && L.Browser.any3d) {
+			L.DomUtil.addClass(this._image, 'leaflet-zoom-animated');
+		} else {
+			L.DomUtil.addClass(this._image, 'leaflet-zoom-hide');
+		}
+
+		this._updateOpacity();
+
+		//TODO createImage util method to remove duplication
+		L.extend(this._image, {
+			galleryimg: 'no',
+			onselectstart: L.Util.falseFn,
+			onmousemove: L.Util.falseFn,
+			onload: L.bind(this._onImageLoad, this),
+			src: this._url
+		});
+	},
+
+	_animateZoom: function (e) {
+		var map = this._map,
+		    image = this._image,
+		    scale = map.getZoomScale(e.zoom),
+		    nw = this._bounds.getNorthWest(),
+		    se = this._bounds.getSouthEast(),
+
+		    topLeft = map._latLngToNewLayerPoint(nw, e.zoom, e.center),
+		    size = map._latLngToNewLayerPoint(se, e.zoom, e.center)._subtract(topLeft),
+		    origin = topLeft._add(size._multiplyBy((1 / 2) * (1 - 1 / scale)));
+
+		image.style[L.DomUtil.TRANSFORM] =
+		        L.DomUtil.getTranslateString(origin) + ' scale(' + scale + ') ';
+	},
+
+	_reset: function () {
+		var image   = this._image,
+		    topLeft = this._map.latLngToLayerPoint(this._bounds.getNorthWest()),
+		    size = this._map.latLngToLayerPoint(this._bounds.getSouthEast())._subtract(topLeft);
+
+		L.DomUtil.setPosition(image, topLeft);
+
+		image.style.width  = size.x + 'px';
+		image.style.height = size.y + 'px';
+	},
+
+	_onImageLoad: function () {
+		this.fire('load');
+	},
+
+	_updateOpacity: function () {
+		L.DomUtil.setOpacity(this._image, this.options.opacity);
+	}
+});
+
+L.imageOverlay = function (url, bounds, options) {
+	return new L.ImageOverlay(url, bounds, options);
+};
