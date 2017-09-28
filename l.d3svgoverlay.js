2a5c440ba6db5d2757a9f894d249c0d7c61e1cbe
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-09-28 14:28:56 -0400

    Map Engine Test Site(Twin Pine) 1.2  /l.d3svgoverlay.js

--- l.d3svgoverlay.js
+++ l.d3svgoverlay.js
@@ -159,7 +159,7 @@ L.D3SvgOverlay = (L.version < "1.0" ? L.Class : L.Layer).extend({
             this.stream.point(point.x, point.y);
         };
         this.projection.pathFromGeojson = d3.geo.path().projection(d3.geo.transform({point: this.projection._projectPoint}));
-        console.log("projection.pathFromGeojson",this.projection.pathFromGeojson);
+        // console.log("projection.pathFromGeojson",this.projection.pathFromGeojson);
         // Compatibility with v.1
         this.projection.latLngToLayerFloatPoint = this.projection.latLngToLayerPoint;
         this.projection.getZoom = this.map.getZoom.bind(this.map);
