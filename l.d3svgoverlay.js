6bff45105bae7d2be012f91c6e4297ce02b58038
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-10-02 12:07:39 -0400

    Map Engine Test Site(Twin Pine) 1.2  /l.d3svgoverlay.js

--- l.d3svgoverlay.js
+++ l.d3svgoverlay.js
@@ -113,9 +113,9 @@ L.D3SvgOverlay = (L.version < "1.0" ? L.Class : L.Layer).extend({
             this._svgGroups = temp_g;
             // this._rootGroup = this._svg.append("g"); 
             this._rootGroup =this._svgGroups; 
-            console.log("this._rootGroup",this._rootGroup);
+            // console.log("this._rootGroup",this._rootGroup);
             
-            console.log("this._svgGroups",this._svgGroups);
+            // console.log("this._svgGroups",this._svgGroups);
         } else {
             this._svg = L.svg();
             map.addLayer(this._svg);
