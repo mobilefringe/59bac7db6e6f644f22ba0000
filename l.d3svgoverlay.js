284c7f590b4d7b837324735f0edb277dab53c491
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-10-03 14:29:15 -0400

    Map Engine Test Site(Twin Pine) 1.2  /l.d3svgoverlay.js

--- l.d3svgoverlay.js
+++ l.d3svgoverlay.js
@@ -216,8 +216,8 @@ L.D3SvgOverlay = (L.version < "1.0" ? L.Class : L.Layer).extend({
 
         L.DomUtil.setTransform(this._svg, offset, scale);
         var shift = ["translate(", offset.x, ",", offset.y, ") "];
-        var scale = ["scale(", tscale, ",", scale,") "];
-        this._svg.attr("transform", shift.concat(scale).join(""));
+        var my_scale = ["scale(", tscale, ",", scale,") "];
+        this._svg.attr("transform", shift.concat(my_scale).join(""));
         // $.each(this._rootGroup, function (key, val){
         //     // console.log(val.id);
         //     if(val.id){
