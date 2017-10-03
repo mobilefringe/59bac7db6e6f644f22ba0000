80faafaaa2b6f2ad9bf82f345c6e26b976da8801
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-10-03 14:51:52 -0400

    Map Engine Test Site(Twin Pine) 1.2  /l.d3svgoverlay.js

--- l.d3svgoverlay.js
+++ l.d3svgoverlay.js
@@ -219,7 +219,7 @@ L.D3SvgOverlay = (L.version < "1.0" ? L.Class : L.Layer).extend({
         var newZoom = e._zoom; // "viewreset" event in Leaflet has not zoom/center parameters like zoomanim
         this._zoomDiff = newZoom - this._zoom;
         this._scale = Math.pow(2, this._zoomDiff);
-        this.projection.scale = this._scale;
+        // this.projection.scale = this._scale;
         this._shift = this.map.latLngToLayerPoint(this._wgsOrigin)
             ._subtract(this._wgsInitialShift.multiplyBy(this._scale*0.1));
         var shift = ["translate(", this._shift.x, ",", this._shift.y, ") "];
