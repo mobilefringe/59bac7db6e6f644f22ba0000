9a3031df50a78e7d12dd10155b6e419fdb4a4adc
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-10-03 14:54:46 -0400

    Map Engine Test Site(Twin Pine) 1.2  /l.d3svgoverlay.js

--- l.d3svgoverlay.js
+++ l.d3svgoverlay.js
@@ -221,7 +221,7 @@ L.D3SvgOverlay = (L.version < "1.0" ? L.Class : L.Layer).extend({
         this._scale = Math.pow(2, this._zoomDiff);
         // this.projection.scale = this._scale;
         console.log(newZoom,this._zoomDiff, this._scale,this._wgsOrigin, e.center );
-        this._shift = this.map.latLngToLayerPoint(this._wgsOrigin)
+        this._shift = this.map.latLngToLayerPoint( e.center)
             ._subtract(this._wgsInitialShift.multiplyBy(this._scale*0.1));
         var shift = ["translate(", this._shift.x, ",", this._shift.y, ") "];
         var scale = ["scale(", this._scale, ",", this._scale,") "];
