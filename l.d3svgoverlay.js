48ecb111113ab0132bbaa0d4af9c864963685011
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-10-04 11:42:20 -0400

    Map Engine Test Site(Twin Pine) 1.2  /l.d3svgoverlay.js

--- l.d3svgoverlay.js
+++ l.d3svgoverlay.js
@@ -84,6 +84,9 @@ L.D3SvgOverlay = (L.version < "1.0" ? L.Class : L.Layer).extend({
         console.log("newZoom",newZoom ,"\n",
         "this._zoomDiff",this._zoomDiff ,"\n",
         "this._scale",this._scale ,"\n",
+         "this._scale",this._scale ,"\n",
+          "this._scale",this._scale ,"\n",
+           "this._scale",this._scale ,"\n",
         "this._shift",this._shift ,"\n",
             "this._shift",this._shift ,"\n"
         );
