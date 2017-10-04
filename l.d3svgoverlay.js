13c4753b8670107cbe21cb92d59c25d81a500e97
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-10-04 12:18:23 -0400

    Map Engine Test Site(Twin Pine) 1.2  /l.d3svgoverlay.js

--- l.d3svgoverlay.js
+++ l.d3svgoverlay.js
@@ -289,7 +289,7 @@ L.D3SvgOverlay = (L.version < "1.0" ? L.Class : L.Layer).extend({
 	},
 	anim_latLngToLayerPoint: function (latLng, zoom) {
         // zoom = _layer._undef(zoom) ? _layer._zoom : zoom;
-        var projectedPoint = _layer.map.project(L.latLng(latLng), zoom)._round();
+        var projectedPoint = this.map.project(L.latLng(latLng), zoom)._round();
         return projectedPoint._subtract(_layer._pixelOrigin);
     }
 
