5665643e5a9f1b4ca423b4d3d23ebd20bd139c76
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-11 11:05:39 -0500

    Map Engine Test Site(Twin Pine) 1.2  /site.js

--- site.js
+++ site.js
@@ -2,7 +2,7 @@ var site_json = {
                     "name" : "Twin Pine",
                     "default_image" : "//codecloud.cdn.speedyrails.net/sites/5890c5296e6f640dd9000000/image/jpeg/1485886146000/twinlogo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "social_feed" : "//longbeach.mallmaverick.com/api/v2/longbeach/social.json"
+                    // "social_feed" : "//longbeach.mallmaverick.com/api/v2/longbeach/social.json"
                 };
 // window.subscribed_store_ids = [];
 // window.subscribed_to_event = false;
